a0bb9decfe34b05b28bd9de8da8d0f673bb90252
Oscar Cappa <cappa.oscar@gmail.com>
2016-12-06 13:03:57 +0100

    Fix README; Del telegram.sh

--- telegram.sh
+++
@@ -1,6 +0,0 @@
-if [ -n "$SSH_CLIENT" ]; then
-  TEXT="%F0%9F%90%BE "`hostname -f`" new connection!%0a"
-  TEXT="$TEXT%E2%9E%96 %F0%9F%91%A4 User: $USER%0A"
-  TEXT="$TEXT%E2%9E%96 %F0%9F%8C%90 IP: "`echo $SSH_CLIENT | awk '{print $1}'`"%0A"
-  /etc/telegram/broadcast.sh "$TEXT"
-fi
